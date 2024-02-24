7c5c81c99ea19f1ede598d2eea29a30fa1d06faa
MarcinSz1993 <marcinsz1993@hotmail.com>
2024-02-24 18:31:52 +0100

    Adding login to application by Spring Security

---
+++ src/main/java/com/example/translator/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.translator.repository;
+
+import com.example.translator.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<UserModel,Integer> {
+
+    Optional<UserModel> findByUsername(String username);
+}
