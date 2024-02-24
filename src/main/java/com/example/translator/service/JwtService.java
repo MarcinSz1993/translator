9e2b61ff6e28213fbca86477f1a20762ef1327a2
MarcinSz1993 <marcinsz1993@hotmail.com>
2024-02-24 18:31:52 +0100

    Adding login to application by Spring Security

---
+++ src/main/java/com/example/translator/service/JwtService.java
@@ -0,0 +1,64 @@
+package com.example.translator.service;
+
+import com.example.translator.model.UserModel;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "2ece0e241248289804d0be313d159e741ddc9a2b02efde6fae83695c300df682";
+
+    public String extractUsername(String token){
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    public boolean isValid(String token, UserDetails userModel){
+        String username = extractUsername(token);
+        return (username.equals(userModel.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver){
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public String generateToken(UserModel userModel){
+        String token = Jwts
+                .builder()
+                .subject(userModel.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 24*60*60*1000))
+                .signWith(getSigningKey())
+                .compact();
+        return token;
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
