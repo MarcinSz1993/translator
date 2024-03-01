24213375a68fa9443e6d3cb567936d7046a3edea
MarcinSz1993 <marcinsz1993@hotmail.com>
2024-03-01 19:06:35 +0100

    Adding addWordToSection method and adding two request classes.

--- src/main/java/com/example/translator/repository/SectionRepository.java
+++ src/main/java/com/example/translator/repository/SectionRepository.java
@@ -4,6 +4,10 @@ import com.example.translator.model.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SectionRepository extends JpaRepository<Section,Integer> {
+
+    Optional<Section> findByName(String sectionName);
 }
