e0626abd484122b2656691e7792eefe468a52d14
MarcinSz1993 <marcinsz1993@hotmail.com>
2024-03-10 11:41:15 +0100

    Adding single_section_view.html and feature adding a word to the section.

--- src/main/java/com/example/translator/service/UserModelService.java
+++ src/main/java/com/example/translator/service/UserModelService.java
@@ -1,9 +1,12 @@
 package com.example.translator.service;
 
+import com.example.translator.model.Section;
 import com.example.translator.model.UserModel;
 import com.example.translator.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +16,13 @@ public class UserModelService {
 
     public UserModel createUser(UserModel userModel){
         return userRepository.save(userModel);
+    }
+
+    public List<String> getWordsFromSection(String username,String sectionName){
+        UserModel user = userRepository.findByUsername(username).orElseThrow();
+        List<Section> list = user.getSections().stream()
+                .filter(section -> section.getName().equals(sectionName))
+                .toList();
+        return list.get(0).getWords();
     }
 }
