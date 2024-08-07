5de31314b6f60d1bf23102c33b11482ee8f2bd1d
MarcinSz1993 <marcinsz1993@hotmail.com>
2024-08-07 09:24:05 +0200

    Modification TranslatorService and adding ApiConfig

--- src/main/java/com/example/translator/service/TranslatorService.java
+++ src/main/java/com/example/translator/service/TranslatorService.java
@@ -1,20 +1,28 @@
 package com.example.translator.service;
 
 import com.example.translator.api.Translatable;
+import com.example.translator.config.ApiConfig;
 import com.example.translator.dto.TranslationDto;
 import com.example.translator.mapper.TranslationMapper;
 import com.example.translator.model.TranslationResponse;
 import com.example.translator.request.DataRequestForTranslation;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class TranslatorService implements Translatable {
     private final WebClient webClient;
-    public TranslatorService(WebClient webClient) {
+    private final ApiConfig apiConfig;
+
+    public TranslatorService(WebClient webClient, ApiConfig apiConfig) {
         this.webClient = webClient;
+        this.apiConfig = apiConfig;
     }
-    public TranslationDto translate(String sourceLanguage,String targetLanguage,DataRequestForTranslation dataRequestForTranslation) {
+    public TranslationDto translate(String sourceLanguage,
+                                    String targetLanguage,
+                                    DataRequestForTranslation dataRequestForTranslation) {
         switch (targetLanguage) {
             case "EN", "PL", "DE","FR" ->
                 dataRequestForTranslation.setTarget_lang(targetLanguage);}
@@ -27,9 +35,9 @@ public class TranslatorService implements Translatable {
 
     private TranslationDto translation(DataRequestForTranslation dataRequestForTranslation) {
         return webClient.post()
-                .uri("https://api-free.deepl.com/v2/translate")
-                .header("Authorization", "DeepL-Auth-Key 5faf23e8-6f6d-4fff-ad4c-6702437406f2:fx")
-                .header("Content-Type", "application/json")
+                .uri(apiConfig.getUri())
+                .header(HttpHeaders.AUTHORIZATION, apiConfig.getApiKey())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .bodyValue(dataRequestForTranslation)
                 .retrieve()
                 .bodyToMono(TranslationResponse.class)
